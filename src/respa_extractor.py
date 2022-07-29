9540ac8ff04f3dafb27aa179cc80954f189df925
christantonis.kons <christantonis.kons@gmail.com>
2022-07-29 15:36:59 +0300

    minors

--- src/respa_extractor.py
+++ src/respa_extractor.py
@@ -1,10 +1,4 @@
-#from re import compile, sub, findall, search, escape, DOTALL, match
-#from collections import OrderedDict
-#from gg18.ggHelper import Helper
-#from gg18.respAclassifier import paragraphClf
 from gg18.respA import RespExtractor
-#from src.fek_parser import FekParser
-
 
 
 class respaExtractor(RespExtractor):
@@ -12,8 +6,10 @@ class respaExtractor(RespExtractor):
     """
     Reusing RespExtractor from gg18
     """
-    def __init__(self, textpath, text):
-        super().__init__(textpath, text)
+    def __init__(self, textpath):
+        super().__init__(textpath)
+    
+    
 
 
 
