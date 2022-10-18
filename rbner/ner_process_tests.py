b9116c5cadd5350b3cd601e6e568f5e217b68022
christantonis.kons <christantonis.kons@gmail.com>
2022-10-18 18:05:58 +0300

    added after #270 the pipeline logic for extracting relations/respas

--- rbner/ner_process_tests.py
+++ rbner/ner_process_tests.py
@@ -265,24 +265,183 @@ i. Ιατρών Ε.Σ.Υ, ii. ειδικευομένων Ιατρών, iii. εξ
 καταπολέμηση των διακρίσεων και την προώθηση των 
 απαραίτητων θεσμικών παρεμβάσεων. """
 
-unit_keywords = ["ΤΜΗΜΑ", "ΓΡΑΦΕΙΟ ", "ΓΡΑΦΕΙΑ ", "ΑΥΤΟΤΕΛΕΣ ", "ΑΥΤΟΤΕΛΗ ", "ΔΙΕΥΘΥΝΣ", "ΥΠΗΡΕΣΙΑ ", "ΣΥΜΒΟΥΛΙ", "ΓΡΑΜΜΑΤΕIA ", "ΥΠΟΥΡΓ", "ΕΙΔΙΚΟΣ ΛΟΓΑΡΙΑΣΜΟΣ", "MONAΔ", "ΠΕΡΙΦΕΡΕΙ"]
+#%%
+from src import dictionaries as dc
 
 
-results = rbner.sentences_with_keywords(a1)
-print(results)
-print(rbner.remove_list_points(a1))
+def remove_first_level(txt):
+    first_line, rest_lines = txt.split("\n", 1)[0], "\n" + txt.split("\n", 1)[1]
+    first_line = re.sub(r"^[ ]*[α-ωΑ-Ω0-9]+[\.\)] ", "", first_line)
+    return first_line + rest_lines
 
+
+def get_paragraph_levels(items):
+    levels = []
+    for i, item in enumerate(items):
+        if item in dc.alphabet:
+            levels.append((item, "alphabet"))  # alphabet
+        elif item in dc.ab_double_combs:
+            levels.append((item, "abcombinations"))  # ab combinations
+        elif item in dc.latin_numbers:
+            levels.append((item, "latinnumbers"))  # latin numbers
+        elif item in dc.numbers:
+            levels.append((item, "therest"))  #
+    return levels
+
+
+def find_levels_depth(txt):
+    split_all = FPRS.split_all_int(txt)
+    split_all = [x for x in split_all if isinstance(x, tuple)]
+
+    split_all = {k:v for k,v in split_all}
+    pointers_list = list(split_all.keys())
     
+    levels = get_paragraph_levels(pointers_list)
+    depth = 1
+    type_of_levels = {levels[0][1]: depth}
+    info = [levels[0] + (depth,) + (split_all[levels[0][0]],)]  
+    for idx, (key, tag) in enumerate(levels[1:]):
+        if tag not in type_of_levels:
+            depth += 1
+            type_of_levels[tag] = depth
+            info.append((key, tag) + (depth,) + (split_all[key],))
+        else:
+            cur_depth = type_of_levels[tag]
+            info.append((key, tag) + (cur_depth,) + (split_all[key],))
+    
+    # reconstructs the flat list of points into groups under the depth = 1
+    split_points = [i for i, (point, typ, dep, text) in enumerate(info) if dep == 1]
+    total_split = split_points + [len(info)]
+    grouped_info = [info[i:j] for i, j in zip(split_points, total_split[1:])]
+
+    return grouped_info, len(type_of_levels)
 
 
+#%% libraries and initialization of objects (common for both tasks)
+
+import time
+start_time = time.time()
+
+from src.fek_parser import PreParser, FekParser
+from src.respa_extractor import RespExtractor
+from rbner.rbNER import rbNER
+import re
+import random
+from rbner.structure import structure
+from rbner.respas import respas
+from collections import OrderedDict
+
+"""give a filepath (in .pdf form)"""
+
+# filepath = "fek-organismoi-upourgeiwn/yp-ygeias-121-2017.pdf"
+filepath = "fek-organismoi-upourgeiwn/yp-metanasteushskaiasulou-106-2020.pdf"
+#filepath = "fek-organismoi-upourgeiwn/yp-tourismou-127-2017.pdf"
+#filepath = "fek-organismoi-upourgeiwn/yp-metanasteushskaiasulou-106-2020.pdf"
+
+"""initialize PreParser that produces a .txt file on directory"""
+text = PreParser().pdf2text(filepath)
+
+"""change the filename extenstion to .txt"""
+textpath = re.sub(r".pdf$", ".txt", filepath)
+
+"""initialize the objects for relation and responsibilities extraction"""
+FPRS, STR, RSP = FekParser(textpath), structure(textpath), respas(textpath)
+rbner = rbNER()
+
+"""get articles on the given text"""
+articles = FPRS.articles
 
 
+#%% relations extraction
+relations_list = []
+for AR_key, AR_value in articles.items():
+    try:
+        article_levels = FPRS.find_article_paragraphs(AR_value)
+        if len(article_levels) > 1:
+            possible_title = article_levels["0"]
+            if any(title_kw in possible_title for title_kw in STR.irrelevant_title):
+                print("Article {} has been skipped due to irrelevant_title".format(AR_key))
+                continue
+            else:        
+                relation_paragraphs = STR.get_paragraphs(article_levels) # returns which paragraphs meet the STRUCTURE requirements
+                relations = STR.get_relations(relation_paragraphs)
+                relations_list.append(relations)
+                print("Article {} has been processed".format(AR_key))
+        
+    except Exception as e:
+        print("Article {} resulted in error".format(AR_key))
+        pass
 
-def clean_up_for_paorgs_getter(txt):
-    return txt.replace('−\n', '').replace('-\n', '')\
-                  .replace('−', '').replace('-', '').replace('\n', ' ')\
-                  .replace(' και ', ' ').replace(' της ', ' ').replace(' του ', ' ').replace(' των ', ' ')\
-                  .replace('  ', ' ').replace('   ', ' ')
+final_list = [x for x in relations_list if x]
+
+
+"""create a graph given the resulted tuples"""
+processed_relations = []
+for lst in final_list:
+    tuplist = []
+    for (x,y) in lst:
+        newvalue = rbner.remove_intonations(y).replace("\n", "").strip()
+        tuplist.append((x, newvalue))
+    processed_relations.append(tuplist)
+
+# for lst in processed_relations:
+#     for tup in lst:
+#         print(tup)
+# processed_relations = [rbner.remove_intonations(t2) for x in final_list for (t1, t2) in x]
+
+graph = STR.get_relations_graph(processed_relations)
+# processed_relations.insert(0, [("ΓΕΝΙΚΗ ΔΙΕΥΘΥΝΣΗ ΤΕΣΤΑΡΙΣΜΑΤΟΣ", "ΔΙΕΥΘΥΝΣΗ ΕΥΡΩΠΑΪΚΗΣ ΚΑΙ ΔΙΕΘΝΟΥΣ ΣΥΝΕΡΓΑΣΙΑΣ"), ("ΓΕΝΙΚΗ ΔΙΕΥΘΥΝΣΗ ΤΕΣΤΑΡΙΣΜΑΤΟΣ", "ΔΙΕΥΘΥΝΣΗ ΥΠΟΔΟΜΩΝ ΠΛΗΡΟΦΟΡΙΚΗΣ ΚΑΙ ΕΠΙΚΟΙΝΩΝΙΩΝ")])
+
+print("Execution time {}".format(time.time() - start_time))
+
+#%% responsibilities extraction
+responsibilities_dict = OrderedDict()
+for AR_key, AR_value in articles.items():
+    try:
+        article_paragraphs = FPRS.find_article_paragraphs(AR_value)
+        if len(article_paragraphs) > 1:
+            possible_title = article_paragraphs["0"]
+            if any(title_kw in possible_title for title_kw in RSP.irrelevant_title):
+                print("Article {} has been skipped due to irrelevant_title".format(AR_key))
+                continue
+            else:
+                responsibility_paragraphs = RSP.get_candidate_paragraphs_per_article(article_paragraphs) #input article text - output list of candidate paragraphs
+                responsibilities = RSP.get_respas(responsibility_paragraphs) # input list of candidate paragraphs - ouput dictionary with unit-respa pairs
+                if responsibilities:
+                    responsibilities_dict[AR_key] = responsibilities
+                    print("We found {} pairs of responsibilities on Article {} has been processed".format(len(responsibilities), AR_key))
+        else:
+            #TODO write rules for 1-paragraph-articles
+            continue
+    except Exception as e:
+        print("Article {} resulted in error".format(AR_key))
+        pass
+
+# final_list = [x for x in responsibilities_list if x]
+
+#%%
+
+article17 = articles["Άρθρο 17"]
+paragraphs = FPRS.find_article_paragraphs(article17)
+paragraphs_list = list(paragraphs.values())
+# paragraph3 = paragraphs["3"]
+paragraphs_list = [paragraphs_list.pop(-2)]
+
+respas = RSP.get_respas(paragraphs_list)
+
+
+new_par3 = remove_first_level(paragraph3)
+
+respas = RSP.get_respas(new_par3)
+
+results = find_levels_depth(new_par3)
+
+if len(paragraphs) > 1:
+    potential_title = paragraphs["0"]
+print(potential_title)
+#%%
+
+
 
 
 
