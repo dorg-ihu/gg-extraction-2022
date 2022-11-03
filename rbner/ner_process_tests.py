a53e5c29f95a96c259171f01f758d5bfee5aeff7
christantonis.kons <christantonis.kons@gmail.com>
2022-11-03 21:18:42 +0200

    minor

--- rbner/ner_process_tests.py
+++ rbner/ner_process_tests.py
@@ -272,8 +272,13 @@ from src import dictionaries as dc
 def remove_first_level(txt):
     first_line, rest_lines = txt.split("\n", 1)[0], "\n" + txt.split("\n", 1)[1]
     first_line = re.sub(r"^[ ]*[α-ωΑ-Ω0-9]+[\.\)] ", "", first_line)
-    return first_line + rest_lines
+    return "\n" + first_line + rest_lines
 
+#TODO check if it is working as expected and add on tool
+def remove_number_level(txt):
+    first_line, rest_lines = txt.split("\n", 1)[0], "\n" + txt.split("\n", 1)[1]
+    first_line = re.sub(r"^[ ]*[0-9]+[\.\)] ", "", first_line)
+    return "\n" + first_line + rest_lines
 
 def get_paragraph_levels(items):
     levels = []
@@ -293,21 +298,20 @@ def find_levels_depth(txt):
     split_all = FPRS.split_all_int(txt)
     split_all = [x for x in split_all if isinstance(x, tuple)]
 
-    split_all = {k:v for k,v in split_all}
-    pointers_list = list(split_all.keys())
+    pointers_list = [x[0] for x in split_all]
     
     levels = get_paragraph_levels(pointers_list)
     depth = 1
     type_of_levels = {levels[0][1]: depth}
-    info = [levels[0] + (depth,) + (split_all[levels[0][0]],)]  
+    info = [levels[0] + (depth,) + (split_all[0][1],)]
     for idx, (key, tag) in enumerate(levels[1:]):
         if tag not in type_of_levels:
             depth += 1
             type_of_levels[tag] = depth
-            info.append((key, tag) + (depth,) + (split_all[key],))
+            info.append((key, tag) + (depth,) + (split_all[idx+1][1],))
         else:
             cur_depth = type_of_levels[tag]
-            info.append((key, tag) + (cur_depth,) + (split_all[key],))
+            info.append((key, tag) + (cur_depth,) + (split_all[idx+1][1],))
     
     # reconstructs the flat list of points into groups under the depth = 1
     split_points = [i for i, (point, typ, dep, text) in enumerate(info) if dep == 1]
@@ -339,7 +343,7 @@ from src.rb_respas_tool import respas
 
 """give a filepath (in .pdf form)"""
 filepath = "fek-organismoi-upourgeiwn/yp-metanasteushskaiasulou-106-2020.pdf"
-
+filepath = "fek-organismoi-upourgeiwn/yp-ergasiaskaikoinwnikhsasfalishs-134-2017.pdf"
 # filepath = "fek-organismoi-upourgeiwn/yp-ygeias-121-2017.pdf"
 
 #filepath = "fek-organismoi-upourgeiwn/yp-tourismou-127-2017.pdf"
@@ -352,7 +356,10 @@ text = PreParser().pdf2text(filepath)
 
 """change the filename extenstion to .txt"""
 textpath = re.sub(r".pdf$", ".txt", filepath)
+
+
 FPRS, RSP = FekParser(textpath), respas(textpath)
+FPRS, STR = FekParser(textpath), structure(textpath)
 """initialize the objects for relation and responsibilities extraction"""
 FPRS, STR, RSP = FekParser(textpath), structure(textpath), respas(textpath)
 
@@ -382,6 +389,43 @@ for AR_key, AR_value in articles.items():
 final_list = [x for x in relations_list if x]
 
 
+
+
+import pydot
+from networkx.drawing.nx_pydot import to_pydot
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+
+interim_relations = [t for sublist in final_list for l in sublist for t in l]
+final_relations = []
+for i in range(0, len(interim_relations), 2):
+    final_relations.append((interim_relations[i], interim_relations[i+1]))
+
+
+the_subject = [x[0] for x in final_relations]
+the_object = [x[1] for x in final_relations]
+
+df = pd.DataFrame({"subject": the_subject,
+                   "object": the_object})
+
+G=nx.from_pandas_edgelist(df, "subject", "object", 
+                  edge_attr=None, create_using=nx.MultiDiGraph())
+plt.figure(figsize=(30,10))
+pos = nx.spring_layout(G)
+nx.draw(G, with_labels=True, node_color='red', edge_cmap=plt.cm.Blues, pos = pos)
+plt.show()
+
+
+
+
+fl = [t for sublist in final_list for l in sublist for t in l]
+tfl = []
+for i in range(0, len(fl), 2):
+    tfl.append((fl[i], fl[i+1]))
+
+
+tfl = list(zip(fl, fl[1:]))
 """create a graph given the resulted tuples"""
 processed_relations = []
 for lst in final_list:
@@ -396,7 +440,7 @@ for lst in final_list:
 #         print(tup)
 # processed_relations = [rbner.remove_intonations(t2) for x in final_list for (t1, t2) in x]
 
-graph = STR.get_relations_graph(processed_relations)
+graph = STR.get_relations_graph(final_list)
 # processed_relations.insert(0, [("ΓΕΝΙΚΗ ΔΙΕΥΘΥΝΣΗ ΤΕΣΤΑΡΙΣΜΑΤΟΣ", "ΔΙΕΥΘΥΝΣΗ ΕΥΡΩΠΑΪΚΗΣ ΚΑΙ ΔΙΕΘΝΟΥΣ ΣΥΝΕΡΓΑΣΙΑΣ"), ("ΓΕΝΙΚΗ ΔΙΕΥΘΥΝΣΗ ΤΕΣΤΑΡΙΣΜΑΤΟΣ", "ΔΙΕΥΘΥΝΣΗ ΥΠΟΔΟΜΩΝ ΠΛΗΡΟΦΟΡΙΚΗΣ ΚΑΙ ΕΠΙΚΟΙΝΩΝΙΩΝ")])
 
 print("Execution time {}".format(time.time() - start_time))
