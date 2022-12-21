c3e35fdde176ac2d6aee982c764a5cb0727f5ce9
christantonis.kons <christantonis.kons@gmail.com>
2022-12-21 12:22:57 +0200

    added method option for RB or ML

--- main.py
+++ main.py
@@ -1,56 +1,71 @@
+from src.fek_parser import PreParser, FekParser
+import argparse
 import os
 import re
-import argparse
-from src.fek_parser import PreParser, FekParser
 
 
-def mainRE(textpath):
-    from rbner.structure import structure
-    FPRS, STR = FekParser(textpath), structure(textpath)
+def mainRE(textpath, method):
+    FPRS = FekParser(textpath)
     articles = FPRS.articles
     
     # keys = ["Άρθρο 2", "Άρθρο 3"] # here read the possible articles that user wants to extract
     # filteredarticles = {key: articles[key] for key in keys if key in articles}
     # relations = STR.main(filteredarticles)
-    relations = STR.main(articles)
-    return relations
+    
+    if method == "RB":
+        from src.rb_structure_tool import structure
+        STR = structure(textpath)
+    else:
+        from src.mlre import structureML as sml
+        STR = sml(textpath)
+    
+    return STR.main(articles)
 
-def mainRSP(textpath):
-    from rbner.respas import respas
-    FPRS, RSP = FekParser(textpath), respas(textpath)
+def mainRSP(textpath, method):
+    FPRS = FekParser(textpath)
     articles = FPRS.articles
     
     # keys = ["Άρθρο 2", "Άρθρο 3"] # here read the possible articles that user wants to extract
     # filteredarticles = {key: articles[key] for key in keys if key in articles}
     # responsibilities = RSP.main(filteredarticles)
-    responsibilities = RSP.main(articles)
-    return responsibilities
+    
+    from src.rb_respas_tool import respas
+    RSP = respas(textpath)
+    
+    # if method == "RB":
+    #     from src.rb_respas_tool import respas
+    #     RSP = respas(textpath)
+    # else:
+    #     from src.mlrsp import respas as rsp
+    #     RSP = rsp(textpath)
+
+    return RSP.main(articles)
     
 
 def main():
     parser = argparse.ArgumentParser(description="Retrieve Information from pdf file")
     parser.add_argument("--filepath", type=str, required=True)
     parser.add_argument("--task", choices=["RE", "RSP"], help="The task of interest to be applied")
+    parser.add_argument("--method", choices=["RB", "ML"], help="The method to perform the task RuleBased or MachineLearning")
     #TODO consider the following in we desire to add option of specific articles
     # parser.add_argument("--articles", nargs="+", type=int, help="A list of integers if you need specific articles only to be processed")
     args = parser.parse_args()
     
-    filepath, task = args.filepath, args.task
+    filepath, task, method = args.filepath, args.task, args.method
     
     pathExists = os.path.exists(filepath)
     if pathExists:
         text = PreParser().pdf2text(filepath)
-        #print(".txt file generated")
+
         #TODO add os-check that file has successfully generated
         textpath = re.sub(r".pdf$", ".txt", filepath)
 
         if task == "RE":
-            results = mainRE(textpath)
+            results = mainRE(textpath, method)
         else:
-            results = mainRSP(textpath)
+            results = mainRSP(textpath, method)
     else:
         print("Invalid filepath")
-    #TODO call a future module to create the required output form - graph/rdf/text etc
 
     print(results, "\n")
     print(f"{len(results)} results found")
@@ -59,4 +74,6 @@ def main():
 if __name__ == '__main__':
     
     main()
+    
+    
     
